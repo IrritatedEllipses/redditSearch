9740db02debaebdd661573aedefdaee28f1ab160
Chris <Christopher.D.Culp@gmail.com>
2018-04-14 00:18:01 -0400

    Using https now

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function showMessage(message, className) {
 
 function search(searchTerm, searchLimit, sortBy) {
         return fetch(
-            `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
+            `https://www.reddit.com/search.json?q=${searchTerm}&sort=${sortBy}&limit=${searchLimit}`
         )
             .then(res => res.json())
             .then(data => data.data.children.map(data => data.data))
